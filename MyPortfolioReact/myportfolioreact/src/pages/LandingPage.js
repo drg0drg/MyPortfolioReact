41e8625e60de929420e68d1092b6396705cf4d0b
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-08-12 15:51:41 +0100

    added about page

--- MyPortfolioReact/myportfolioreact/src/pages/LandingPage.js
+++ MyPortfolioReact/myportfolioreact/src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavbarLanding from "../components/NavbarLanding";
+import AboutMe from "../components/Aboutme";
 import "../assets/style/style.css";
 
 const LandingPage = () => {
 	return (
 		<div>
 			<NavbarLanding />
+      <AboutMe/>
 		</div>
 	);
 };
