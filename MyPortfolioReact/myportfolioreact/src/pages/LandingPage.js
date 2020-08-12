c8ec0564235d24dbca20809186d2d14584ace76d
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-08-12 14:18:25 +0100

    created landing page and navbar

---
+++ MyPortfolioReact/myportfolioreact/src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarLanding from "../components/NavbarLanding";
+import "../assets/style/style.css";
+
+const LandingPage = () => {
+	return (
+		<div>
+			<NavbarLanding />
+		</div>
+	);
+};
+
+export default LandingPage;
